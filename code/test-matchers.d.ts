0e31db9a1607a555cec52aefa4a652a85431dd8e
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2026-03-24 13:52:08 +0100

    Also define extended matcher in root folder

---
+++ code/test-matchers.d.ts
@@ -0,0 +1,4 @@
+// Allows CI type check to find extend-to-be-announced custom matchers.
+// The CI environment resolves tsconfig differently from IDEs so this is
+// purposefully redundant with the .d.ts found in the addon-vitest folder.
+import 'extend-to-be-announced/vitest';
